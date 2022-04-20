3d06567fe6934cf817773ad8b665f51e86f97a79
Cristian Pineda <cristianpined4@cristianpineda2>
2022-04-19 19:57:20 -0600

    Primer comit modulo de inventario y proveedores

---
+++ dbsistema.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 19-04-2022 a las 19:26:10
+-- Versión del servidor: 10.3.34-MariaDB-0ubuntu0.20.04.1
+-- Versión de PHP: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistemafac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `idventa` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) DEFAULT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) DEFAULT NULL,
+  `total_venta` decimal(11,2) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idventa`),
+  ADD KEY `fk_venta_persona_idx` (`idcliente`),
+  ADD KEY `fk_venta_usuario_idx` (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idventa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `fk_venta_persona` FOREIGN KEY (`idcliente`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ vistas/header.php
@@ -0,0 +1,159 @@
+ <?php
+  include_once "../config/global.php";
+  if (strlen(session_id()) < 1)
+    session_start();
+
+	$pathName = explode("/",$_SERVER["REQUEST_URI"])[PATH_INDEX];
+  ?>
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <title><?php echo ucfirst(strtolower(str_replace("%20"," ",$pathName))); ?> | <?php echo PRO_NOMBRE ?></title>
+   <!-- Tell the browser to be responsive to screen width -->
+   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+   <!-- Bootstrap 3.3.7 -->
+   <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+   <!-- Font Awesome -->
+
+   <link rel="stylesheet" href="../public/css/font-awesome.min.css">
+
+   <link rel="stylesheet" href="../public/css/AdminLTE.min.css">
+   <link rel="stylesheet" href="../public/css/_all-skins.min.css">
+   <!-- Morris chart -->
+   <!-- Daterange picker -->
+   <link rel="stylesheet" href="img/apple-touch-ico.png">
+   <link rel="stylesheet" href="img/favicon.ico">
+   <!-- DATATABLES-->
+   <link rel="stylesheet" href="../public/datatables/jquery.dataTables.min.css">
+   <link rel="stylesheet" href="../public/datatables/buttons.dataTables.min.css">
+   <link rel="stylesheet" href="../public/datatables/responsive.dataTables.min.css">
+   <link rel="stylesheet" href="../public/css/bootstrap-select.min.css">
+
+ </head>
+
+ <body class="hold-transition skin-blue sidebar-mini">
+   <div class="wrapper">
+
+     <header class="main-header">
+       <!-- Logo -->
+       <a href="escritorio" class="logo">
+         <!-- mini logo for sidebar mini 50x50 pixels -->
+         <span class="logo-mini text-uppercase"><b><?php echo PRO_NOMBRE ?></b></span>
+         <!-- logo for regular state and mobile devices -->
+         <span class="logo-lg text-uppercase"><?php echo PRO_NOMBRE ?></span>
+       </a>
+       <!-- Header Navbar: style can be found in header.less -->
+       <nav class="navbar navbar-static-top">
+         <!-- Sidebar toggle button-->
+         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+           <span class="sr-only">NAVEGACIÓM</span>
+         </a>
+
+         <div class="navbar-custom-menu">
+           <ul class="nav navbar-nav">
+
+             <li class="dropdown user user-menu">
+               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                 <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="user-image" alt="User Image">
+                 <span class="hidden-xs"><?php echo $_SESSION['nombre']; ?></span>
+               </a>
+               <ul class="dropdown-menu">
+                 <!-- User image -->
+                 <li class="user-header">
+                   <img src="../files/usuarios/<?php echo $_SESSION['imagen']; ?>" class="img-circle" alt="User Image">
+
+                   <p>
+                     <?php echo $_SESSION['nombre']; ?>
+                     <small><?php echo $_SESSION['cargo']; ?></small>
+                   </p>
+                 </li>
+                 <!-- Menu Footer-->
+                 <li class="user-footer">
+                   <div class="pull-left">
+
+                   </div>
+                   <div class="pull-right">
+                     <a href="../ajax/usuario?op=salir" class="btn btn-default btn-flat">Salir</a>
+                   </div>
+                 </li>
+               </ul>
+             </li>
+             <!-- Control Sidebar Toggle Button -->
+
+           </ul>
+         </div>
+       </nav>
+     </header>
+     <!-- Left side column. contains the logo and sidebar -->
+     <aside class="main-sidebar">
+       <!-- sidebar: style can be found in sidebar.less -->
+       <section class="sidebar">
+         <!-- Sidebar user panel -->
+
+         <!-- /.search form -->
+         <!-- sidebar menu: : style can be found in sidebar.less -->
+         <ul class="sidebar-menu" data-widget="tree">
+
+           <br>
+           <?php
+            if ($_SESSION['escritorio'] == 1) {
+              echo ' <li><a href="escritorio"><i class="fa  fa-dashboard (alias)"></i> <span>Escritorio</span></a>
+        </li>';
+            }
+            ?>
+           <?php
+            if ($_SESSION['almacen'] == 1) {
+              echo ' <li class="treeview">
+          <a href="#">
+            <i class="fa fa-laptop"></i> <span>Almacen</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="articulo"><i class="fa fa-circle-o"></i> Articulos</a></li>
+            <li><a href="categoria"><i class="fa fa-circle-o"></i> Categorias</a></li>
+          </ul>
+        </li>';
+            }
+            ?>
+           <?php
+            if ($_SESSION['compras'] == 1) {
+              echo ' <li class="treeview">
+          <a href="#">
+            <i class="fa fa-th"></i> <span>Compras</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="ingreso"><i class="fa fa-circle-o"></i> Ingresos</a></li>
+            <li><a href="proveedor"><i class="fa fa-circle-o"></i> Proveedores</a></li>
+          </ul>
+        </li>';
+            }
+            ?>
+
+           <?php
+            if ($_SESSION['acceso'] == 1) {
+              echo '  <li class="treeview">
+          <a href="#">
+            <i class="fa fa-folder"></i> <span>Acceso</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="usuario"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+            <li><a href="permiso"><i class="fa fa-circle-o"></i> Permisos</a></li>
+          </ul>
+        </li>';
+            }
+            ?>
+         </ul>
+       </section>
+       <!-- /.sidebar -->
+     </aside>
